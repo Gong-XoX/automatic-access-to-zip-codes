477329f89bd0bf4e544cc261d1fa91d150ad0332
gongxinxin <gong.xinxin@foxmail.com>
2021-12-19 17:13:13 +0800

    修改调用方法

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 (async () => {
+  const baseURL = 'http://120.24.46.229:8000/';
   async function fetch(url, data, methods = 'GET') {
     const baseConfig = {
-      baseURL: 'http://120.24.46.229:8000/',
+      baseURL,
       timeout: 10000,
       responseType: 'json',
       // withCredentials: true,
@@ -25,9 +26,11 @@
       queryZipCode();
     }
   });
+
   queryBtn.click(async function () {
     queryZipCode();
   });
+
   async function queryZipCode() {
     const value = address[0].value;
     if (!value) {
@@ -37,16 +40,20 @@
     const { code, data } = res;
     if (code === '000' && data && data.postcode) {
       const code = data.postcode;
-      let input = document.createElement('input');
-      input.value = code;
-      document.body.appendChild(input);
-      input.select();
-      document.execCommand('copy');
-      document.body.removeChild(input);
-      zipCode[0].value = data.postcode;
-      message.run('已成功复制到剪切板');
+      copy(code);
+      zipCode[0].value = code;
     } else {
       message.run('查询失败, 请重新填写地址', 'error');
     }
+  }
+
+  function copy(value) {
+    let input = document.createElement('input');
+    input.value = value;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+    message.run('已成功复制到剪切板');
   }
 })();
