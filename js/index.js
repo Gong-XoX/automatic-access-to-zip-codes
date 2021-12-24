90d370af49c945d9d361b57dec225a1955b653dd
gongxinxin <gong.xinxin@foxmail.com>
2021-12-24 12:20:57 +0800

    添加js注释

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 (async () => {
+  // axios请求
   const baseURL = 'http://120.24.46.229:8000/';
   async function fetch(url, data, methods = 'GET') {
     const baseConfig = {
@@ -29,19 +30,20 @@
       }, time);
     };
   }
-
+  // 获取页面元素
   const address = $('.address');
   const zipCode = $('.zipCode');
   const queryBtn = $('.query-btn');
+  // 输入框地址回车事件
   address.keyup(function (e) {
     const key = e.which;
     if (key === 13) {
       queryZipCode();
     }
   });
-
+  // 点击查询事件
   queryBtn.click(debounce(queryZipCode, 500));
-
+  // 查询邮政编码
   async function queryZipCode() {
     const value = address[0].value;
     if (!value) {
@@ -57,7 +59,7 @@
       message.run('查询失败, 请重新填写地址', 'error');
     }
   }
-
+  // 复制剪切板
   function copy(value) {
     let input = document.createElement('input');
     input.value = value;
